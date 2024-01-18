f00237be1d85e8d981c83f4ce78e3ff4b7413cb4
Omar Faruk <omarfaruk65142@gmail.com>
2024-01-18 17:35:48 +0600

    Implement the food menu section

--- src/components/Shared/Menu/Menu.jsx
+++ src/components/Shared/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import { Col, Container, Nav, Row } from "react-bootstrap";
 import { RiCupFill } from "react-icons/ri";
 import { FaHamburger } from "react-icons/fa";
-import { FaUtensils } from "react-icons/fa6";
+import { FaUtensils } from "react-icons/fa";
 import MenuOne from "./../../../assets/menu-1.jpg"
 import MenuTwo from "./../../../assets/menu-2.jpg"
 import MenuThree from "./../../../assets/menu-3.jpg"
@@ -11,19 +11,35 @@ import MenuSix from "./../../../assets/menu-6.jpg"
 import MenuSeven from "./../../../assets/menu-7.jpg"
 import MenuEight from "./../../../assets/menu-8.jpg"
 import "./Menu.css"
-import MenuIteams from "./MenuIteams/MenuIteams";
+import MenuIteams from "./MenuIteams/MenuIteams.jsx";
+import { useState } from "react";
 
 const Menu = () => {
-    const menu = [
+    const menuBreakFast = [
         { id: 1, img: MenuOne, title: "Chicken Burger", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 120 },
-        { id: 2, img: MenuFour, title: "Chicken Parmesan", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 105 },
+        { id: 2, img: MenuTwo, title: "Chicken Parmesan", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 105 },
         { id: 3, img: MenuThree, title: "Chicken Burger", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 110 },
         { id: 4, img: MenuFour, title: "Chicken Burger", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 125 },
         { id: 5, img: MenuFive, title: "Chicken Burger", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 120 },
         { id: 6, img: MenuSix, title: "Chicken Parmesan", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 105 },
         { id: 7, img: MenuSeven, title: "Chicken Burger", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 110 },
         { id: 8, img: MenuEight, title: "Chicken Burger", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 125 },
-    ]
+    ];
+    const menuLaunch = [
+        { id: 1, img: MenuEight, title: "Chicken Burger", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 125 },
+        { id: 2, img: MenuSeven, title: "Chicken Burger", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 110 },
+        { id: 3, img: MenuSix, title: "Chicken Parmesan", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 105 },
+        { id: 4, img: MenuFive, title: "Chicken Burger", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 120 },
+        { id: 7, img: MenuTwo, title: "Chicken Parmesan", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 105 },
+        { id: 5, img: MenuFour, title: "Chicken Burger", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 125 },
+        { id: 6, img: MenuThree, title: "Chicken Burger", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 110 },
+        { id: 8, img: MenuOne, title: "Chicken Burger", details: "Ipsum ipsum clita erat amet dolor justo diam", price: 120 },
+    ];
+    const [activeTab, setActiveTab] = useState("tab-1");
+
+    const handleTabClick = (tabKey) => {
+        setActiveTab(tabKey);
+    };
     return (
         <section className="container-xxl py-5">
             <Container className="text-center">
@@ -31,9 +47,9 @@ const Menu = () => {
                     <h5 className="text-center">Food Menu</h5>
                     <h1 className="mb-5">Most Popular Items</h1>
                 </div>
-                <Nav className="nav-pills d-inline-flex justify-content-center border-bottom mb-5 align-items-center">
+                <Nav className="nav-pills d-inline-flex justify-content-center border-bottom mb-5 align-items-center" activeKey={activeTab}>
                     <Nav.Item>
-                        <Nav.Link className="d-flex align-items-center text-start mx-3 pb-3" eventKey="tab-1">
+                        <Nav.Link className="d-flex align-items-center text-start mx-3 pb-3" eventKey="tab-1" onClick={() => handleTabClick("tab-1")}>
                             < RiCupFill className="menu-logo" />
                             <div className="ps-3 menu-items-title">
                                 <small className="text-body">Popular</small>
@@ -42,7 +58,7 @@ const Menu = () => {
                         </Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link className="d-flex align-items-center text-start mx-3 pb-3" eventKey="tab-2">
+                        <Nav.Link className="d-flex align-items-center text-start mx-3 pb-3" eventKey="tab-2" onClick={() => handleTabClick("tab-2")}>
                             < FaHamburger className="menu-logo" />
                             <div className="ps-3 menu-items-title">
                                 <small className="text-body">Special</small>
@@ -51,7 +67,7 @@ const Menu = () => {
                         </Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link className="d-flex align-items-center text-start mx-3 me-0 pb-3" eventKey="tab-3">
+                        <Nav.Link className="d-flex align-items-center text-start mx-3 me-0 pb-3" eventKey="tab-3" onClick={() => handleTabClick("tab-3")}>
                             <FaUtensils className="menu-logo"></FaUtensils>
                             <div className="ps-3 menu-items-title">
                                 <small className="text-body">Lovely</small>
@@ -60,15 +76,45 @@ const Menu = () => {
                         </Nav.Link>
                     </Nav.Item>
                 </Nav>
-                <Row>
-
-                    {menu.map(data => {
-                         <Col key={data.id} md={6}>
-                         <MenuIteams data={data} />
-                     </Col>
-                    })}
-
-                </Row>
+                {activeTab === "tab-1" && (
+                    <Row id="tab-1" className="tab-pane g-1">
+                        {menuBreakFast.map((data) => (
+                            <Col
+                                key={data.id}
+                                lg={6}
+                                className="align-item-start text-start d-flex menu-items-wrap"
+                            >
+                                <MenuIteams data={data} />
+                            </Col>
+                        ))}
+                    </Row>
+                )}
+                {activeTab === "tab-2" && (
+                    <Row id="tab-2" className="tab-pane g-1">
+                        {menuLaunch.map((data) => (
+                            <Col
+                                key={data.id}
+                                lg={6}
+                                className="align-item-start text-start d-flex menu-items-wrap"
+                            >
+                                <MenuIteams data={data} />
+                            </Col>
+                        ))}
+                    </Row>
+                )}
+                {activeTab === "tab-3" && (
+                    <Row id="tab-3" className="tab-pane g-1">
+                        {menuBreakFast.map((data) => (
+                            <Col
+                                key={data.id}
+                                lg={6}
+                                className="align-item-start text-start d-flex menu-items-wrap"
+                            >
+                                <MenuIteams data={data} />
+                            </Col>
+                        ))}
+                    </Row>
+                )}
             </Container>
         </section>
     );
--- src/components/Shared/Menu/MenuIteams/MenuIteams.jsx
+++ src/components/Shared/Menu/MenuIteams/MenuIteams.jsx
@@ -1,10 +1,25 @@
+import { Card } from "react-bootstrap";
 import "./MenuIteams.css"
-const MenuIteams = ({data}) => {
+const MenuIteams = ({ data }) => {
     console.log(data)
+    const { img, title, details, price } = data;
     return (
-        <div>
-            
-        </div>
+        <Card className="menu-items-wrap w-100">
+            <Card.Body className="d-flex">
+                <div className="image justify-content-center mt-3">
+                    <img src={img} alt="" className="img-fluid rounded"/>
+                </div>
+                <div className="menu-details w-100 d-flex flex-column text-start ps-4" style={{width: "80px"}}>
+                    <div className="d-flex justify-content-between border-bottom pb-2">
+                        <h5>{title}</h5>
+                        <p className="price">${price}</p>
+                    </div>
+                    <div>
+                        <p className="menu-text-details">{details}</p>
+                    </div>
+                </div>
+            </Card.Body>
+        </Card>
     );
 };
 
